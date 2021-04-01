7450499ddd03db63143c44cd4920645f03517aa9
unknown <katalin.varga@students.plymouth.ac.uk>
2021-04-01 23:14:26 +0100

    made it responsive from 936px width and added a js function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 
+function changeImage() {                    /* based on an example at https://wlearnsmart.com/javascript-change-image-onclick-event/ */
+
+    var Image_Id = document.getElementById('glassJar'); /*queries the id of the image element*/
+    if (Image_Id.src.match("img/jar.jpg")) {      /* if this is the source for the jar image*/
+        Image_Id.src = "img/yoganbox.png";          /* than it changes the source to the image of the Yogan box*/
+    }
+    else {
+        Image_Id.src = "img/jar.jpg";           /* otherwise it stays the jar image - if function is not activated - it is activated by hovering over the image as defined in the html tag*/
+    }
+} 
